5b649e91855b6ddbffeef0dc505561ca9d193bd5
chanwoo <66253711+chanwoo000@users.noreply.github.com>
2024-10-23 04:02:59 +0900

    [Gold V] Title: LCS, Time: 476 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,11 @@
+word1 = list(input())
+word2 = list(input())
+lcs = [[0] * (len(word2) + 1) for _ in range(len(word1) + 1)]
+for i in range(1,len(word1)+1):
+    for j in range(1,len(word2)+1):
+        if word1[i-1] == word2[j-1]:
+            lcs[i][j] = lcs[i-1][j-1] + 1
+        else:
+            lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
+            
+print(max(map(max,lcs)))
