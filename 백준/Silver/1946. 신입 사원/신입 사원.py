4df1cb93538b981f43ca16d78bdcf02254ebd5cd
chanwoo <66253711+chanwoo000@users.noreply.github.com>
2024-11-26 23:05:27 +0900

    [Silver I] Title: 신입 사원, Time: 2184 ms, Memory: 207332 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,20 +1,19 @@
 import sys
+n = int(input())
+tmp = []
 
-input = sys.stdin.readline
-T = int(input())
-
-for tc in range(T):
-    N = int(input())
-    freshman = [list(map(int, input().split())) + [i + 1] for i in range(N)]
-
-    freshman.sort(key=lambda x: x[0])
-
-    rank = freshman[0][1]  
-    cnt = 1  
-
-    for i in range(1, N):
-        if rank > freshman[i][1]:  
-            rank = freshman[i][1]  
-            cnt += 1  
-
+for i in range(n):
+    a = int(input())
+    arr = []
+    for j in range(a):
+        b,c = map(int,sys.stdin.readline().split())
+        arr.append((b,c))
+    arr.sort(key=lambda x : x[0])
+    two = arr[0][1]
+    cnt = 1
+    for k in range(1,a):
+        if arr[k][1] < two :
+            two = arr[k][1]
+            cnt += 1
     print(cnt)
+
